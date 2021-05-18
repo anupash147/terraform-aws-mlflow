93c446f864849622c36cf76cde4d3ddf5d8537f0
anupash147 <anupash147@yahoo.com>
2021-05-18 13:55:04 -0500

    minor bug fix

--- locals.tf
+++ locals.tf
@@ -3,11 +3,6 @@ locals {
   db_port                 = 3306
   create_dedicated_bucket = var.artifact_bucket_id == null
   artifact_bucket_id      = local.create_dedicated_bucket ? aws_s3_bucket.default.0.id : var.artifact_bucket_id
-  tags = merge(
-    {
-      "terraform-module" = "glovo/mlflow/aws"
-    },
-    var.tags
-  )
+  tags = var.tags
 }
 
